9fd5ee0cfa3cb6a2cbd0ee964477c753d56c9a51
Silva Véronique, GHR-POD-NEX-31 <Veronique.Silva@swisscom.com>
2023-03-01 18:12:08 +0100

    startPomo() working

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="timer">
+
+  <div class="title">Pomodoro app</div>
+
+  <button @click="startPomo">Start</button>
+  <button @click="stopPomo">Stop</button>
+  <button @click="reset">Set Time</button>
+
+ </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      timer: null,
+      pomoTime: 10000,
+    }
+  },
+  methods: {
+    startPomo() {
+      console.log('entering startPomo');
+      this.timer = setInterval(() => {
+        console.log('before the while');
+        do {
+          this.pomoTime -= 1000;
+        } while (this.pomoTime <= 0);
+        console.log('I am in the startPomo method: ' + this.pomoTime);
+      }, 1000);
+      console.log('I am at the end of the startPomo.')
+    },
+    stopPomo() {
+      clearInterval(this.timer);
+    },
+    reset() {}
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.timer {
+  width: 400px;
+  border-radius: 20px;
+  background: #182bba;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+
+.title {
+  vertical-align: top;
 }
 </style>
