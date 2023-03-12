9ff39f328caab25e3e80d45d9d7d3da9e8232bde
Silva Véronique, GHR-POD-NEX-31 <Veronique.Silva@swisscom.com>
2023-03-12 10:42:44 +0100

    Deleted reset button, change in CSS

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
  <div class="timer">
   <div class="title">Pomodoro</div>
-  <div class="input" v-if="showInput">
-    <form>
-      <label>Work Time in ms </label>
-      <input type="number" v-model="workTime"><br>
-      <label>Chill Out Time in ms </label>
-      <input type="number" v-model="chillOutTime">
-    </form>
-  </div>
+    <!--https://sabe.io/blog/javascript-convert-milliseconds-seconds-minutes-hours and stackoverflow for the zero before seconds-->
+    <p class="clock">{{ Math.floor((workTime / 1000 / 60) % 60) + (Math.floor((workTime / 1000) % 60) < 10 ? (" : 0" + Math.floor((workTime / 1000) % 60))
+        : (" : " + Math.floor((workTime / 1000) % 60)))}}</p>
+    <div class="input" v-if="showInput">
+      <form>
+        <label>Work Time in ms </label>
+        <input type="number" v-model="workTime"><br>
+        <label>Chill Out Time in ms </label>
+        <input type="number" v-model="chillOutTime">
+      </form>
+    </div>
     <div v-if="showWork">
       <div class="buttonContainer">
         <button @click="startWork">Start Work</button>
         <button @click="stopWork">Stop Work</button>
-        <button @click="resetWork">Reset Standard</button>
-      </div>
-      <!--https://sabe.io/blog/javascript-convert-milliseconds-seconds-minutes-hours and stackoverflow for the zero before seconds-->
-      <p class="clock">{{ Math.floor((workTime / 1000 / 60) % 60) + (Math.floor((workTime / 1000) % 60) < 10 ? (" : 0" + Math.floor((workTime / 1000) % 60))
-      : (" : " + Math.floor((workTime / 1000) % 60)))}}</p>
+      </div>  
     </div>
-    <div v-if="showChillOut">
+    <div v-if="showChillOut" class="pause">
       <div class="buttonContainer">
         <button @click="startChillOut">Start Chill Out</button>
         <button @click="stopChillOut">Stop Chill Out</button>
-        <button @click="resetChillOut">Reset Standard</button>
       </div>
       <p class="clock">{{ Math.floor((chillOutTime / 1000 / 60) % 60) + (Math.floor((chillOutTime / 1000) % 60) < 10 ? (" : 0" + Math.floor((chillOutTime / 1000) % 60))
       : (" : " + Math.floor((chillOutTime / 1000) % 60)))}}</p>
+      <p v-if="endChill">End of ChillOut</p>
     </div>
  </div> 
 </template>
@@ -45,7 +44,8 @@ export default {
       chillOutTime: null,
       showInput: true,
       showWork: true,
-      showChillOut: false
+      showChillOut: false,
+      endChill: false
     }
   },
   methods: {
@@ -71,9 +71,8 @@ export default {
       console.log('stopping');
     },
     resetWork() {
-      //this.showInput = true;
-      this.workTime = 60000; //how to reset to value entered by user ?
-      console.log('Reset to ms: ' + this.workTime);
+       this.workTime = 10000; //how to reset to value entered by user ?
+       console.log('Reset to ms: ' + this.workTime);
     },
     startChillOut() {
       this.showInput = false;
@@ -87,6 +86,7 @@ export default {
           this.resetWork();
           this.resetChillOut();
           this.showInput = true;
+          this.endChill = true;
         }
       }, 1000)
     },
@@ -95,9 +95,9 @@ export default {
       console.log('stopping chill out');
     },
     resetChillOut() {
-      this.chillOutTime = 20000;
-      console.log('Reset chill out to ms: ' + this.chillOutTime);
-    }
+       this.chillOutTime = 5000;
+       console.log('Reset chill out to ms: ' + this.chillOutTime);
+     }
   }
 }
 </script>
@@ -116,8 +116,8 @@ export default {
   width: 25vw;
   height: 30vh;
   border-radius: 20px;
-  background: #182bba;
-  color: white;
+  background: #1BD7DF;
+  color: #FFFFF0;
   padding:  5vh 5vw 15vh 5vw;
   margin: 3vh 3vw;
 }
@@ -151,6 +151,15 @@ button {
   border: 2px solid white;
   padding: 4vh;
   font-size: 15px;
+}
+.pause {
+  width: 80vw;
+  height: 80vh;
+  border-radius: 20px;
+  color: #FFFFF0;
+  padding:  5vh 5vw 15vh 5vw;
+  margin: 3vh 3vw;
+  background:#ed1dbf
 }
 
 </style>
